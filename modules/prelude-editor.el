d26924894b31d5dc3a8b2813719579baccc2b433
Sorin Ionescu <sorin.ionescu@gmail.com>
2011-10-16 15:47:39 -0400

    Added support for Mac OS X clipboard.

--- modules/prelude-editor.el
+++ modules/prelude-editor.el
@@ -169,6 +169,20 @@
 (add-hook 'before-save-hook
           'delete-trailing-whitespace)
 
+;; mac os x clipboard
+(when (string= system-type "darwin")
+  (defun copy-from-osx ()
+    (shell-command-to-string "pbpaste"))
+
+  (defun paste-to-osx (text &optional push)
+    (let ((process-connection-type nil))
+      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+        (process-send-string proc text)
+        (process-send-eof proc))))
+
+  (setq interprogram-cut-function 'paste-to-osx)
+  (setq interprogram-paste-function 'copy-from-osx))
+
 ;; projectile is a project management mode
 (require 'projectile)
 (projectile-global-mode t)
