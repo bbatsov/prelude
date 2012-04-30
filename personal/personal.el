c17c5a2d4f455788f8b7d875b02e03e1b6dd24b1
Abizer Nasir <abizern@abizern.org>
2012-04-30 16:06:48 +0100

    Set ido-mode as the default YASnippet prompt

--- personal/personal.el
+++ personal/personal.el
@@ -51,12 +51,17 @@
 ;;; I like to use shell, not eshell
 (global-set-key (kbd "C-x m") 'shell)
 
+;; YASnippet
+
 ;;; Remove binding of <tab> key from Markdown cycle
 ;;; This way I can use YASnippets in my markdown files.
 (defun markdown-unset-tab ()
   "markdown-mode-hook"
   (define-key markdown-mode-map (kbd "<tab>") nil))
 (add-hook 'markdown-mode-hook '(lambda() (markdown-unset-tab)))
+
+;;; Use ido-mode for choosing snippets
+(setq yas/prompt-functions '(yas/ido-prompt yas/dropdown-prompt))
 
 ;; Enable erase-buffer
 (put 'erase-buffer 'disabled nil)
