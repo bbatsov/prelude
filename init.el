b7ab712e201d88bfca310995dd1f55ee04bfba39
bas <fbs.devv@gmail.com>
2012-06-21 01:13:39 +0200

    Added a 'module' that starts an emacs-server when non is running.

--- init.el
+++ init.el
@@ -61,6 +61,7 @@ ELPA (or MELPA).")
 (require 'prelude-mode)
 (require 'prelude-editor)
 (require 'prelude-global-keybindings)
+(require 'prelude-server)
 
 ;; OSX specific settings
 (when (eq system-type 'darwin)
