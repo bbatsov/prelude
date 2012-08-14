e83708edfdea9a055991cf952ab4ac5578821190
Anton Ilin <anton.ilin@anahoret.com>
2012-08-14 18:29:40 +0300

    Setup mmm-mode for html-editing.

--- personal/rc-ror-development.el
+++ personal/rc-ror-development.el
@@ -7,3 +7,13 @@
   (package-install 'mmm-mode))
 
 (require 'mmm-mode)
+(setq mmm-global-mode 'auto)
+
+;; configure html editing
+(mmm-add-mode-ext-class 'html-erb-mode nil 'html-js)
+(mmm-add-mode-ext-class 'html-erb-mode nil 'html-css)
+(mmm-add-mode-ext-class 'html-erb-mode "\\.html\\.erb\\'" 'erb)
+(mmm-add-mode-ext-class 'html-erb-mode "\\.jst\\.ejs\\'" 'ejs)
+
+(add-to-list 'auto-mode-alist '("\\.html\\.erb\\'" . html-erb-mode))
+(add-to-list 'auto-mode-alist '("\\.jst\\.ejs\\'" . html-erb-mode))
