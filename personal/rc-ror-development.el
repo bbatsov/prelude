877af94353fc4d0286538cf88ba0e525f17f7e8e
Anton Ilin <anton.ilin@anahoret.com>
2012-08-14 17:52:51 +0300

    Add mmm-mode package.

--- personal/rc-ror-development.el
+++ personal/rc-ror-development.el
@@ -1,3 +1,9 @@
 ;; setup rinari package
 (add-to-list 'load-path "~/.emacs.d/vendor/rinari")
 (require 'rinari)
+
+(unless (package-installed-p 'mmm-mode)
+  (package-refresh-contents)
+  (package-install 'mmm-mode))
+
+(require 'mmm-mode)
