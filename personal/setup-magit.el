3bbbf8ebcfa74aed0aadb2e72b36b208b797cfa5
Abizer Nasir <abizern@abizern.org>
2012-12-23 14:10:03 +0000

    Add a full-screen mode for magit

---
+++ personal/setup-magit.el
@@ -0,0 +1,17 @@
+;; Full screen magit-status
+;;; Taken from http://whattheemacsd.com/setup-magit.el-01.html
+
+(require 'magit)
+
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
