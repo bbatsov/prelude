1d73e949b53786daac7f77a984547279e849a3cb
Abizer Nasir <abizern@abizern.org>
2013-02-11 00:48:45 +0000

    Comment shortcuts for personal-defuns

--- personal/personal-defuns.el
+++ personal/personal-defuns.el
@@ -3,6 +3,7 @@
 ;; I might want to shift this into a minor mode when I learn how to do that.
 ;;
 
+;; Shortcut C-S-return
 (defun newline-previous ()
   "Insert a blank line above the cursor and move the cursor up one line."
   (interactive)
@@ -11,6 +12,7 @@
   (previous-line)
   (indent-according-to-mode))
 
+;; Shortcut C-return
 (defun newline-next ()
   "Inserts an indented newline after the current line and moves the point to it"
   (interactive)
