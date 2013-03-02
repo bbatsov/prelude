05ae4d7120188f1868a984dd174f1dc7e0a3d86d
Abizer Nasir <abizern@abizern.org>
2013-03-02 23:11:18 +0000

    Better layout of code in Personal-defuns.el

--- personal/personal-defuns.el
+++ personal/personal-defuns.el
@@ -1,7 +1,11 @@
-;;; personal-defuns.el --- elisp helper functions
+;;; personal-defuns.el --- My elisp helper functions
 ;;
 ;; I might want to shift this into a minor mode when I learn how to do that.
 ;;
+;;; Commentary:
+;; Convenience defuns that I sue myself
+
+;;; Code:
 
 ;; Shortcut C-S-return
 (defun newline-previous ()
@@ -14,7 +18,10 @@
 
 ;; Shortcut C-return
 (defun newline-next ()
-  "Inserts an indented newline after the current line and moves the point to it"
+  "Insert an indented newline after the current line and move the point to it."
   (interactive)
   (end-of-line)
   (newline-and-indent))
+
+(provide 'personal-defuns)
+;;; personal-defuns.el ends here
