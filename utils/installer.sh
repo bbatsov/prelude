dd0bfbb0a01d6a8d8106d6f307987adaffb54fb5
Abizer Nasir <abizern@abizern.org>
2013-05-05 09:00:55 +0100

    Use my url for installer.sh rather than the upstream

--- utils/installer.sh
+++ utils/installer.sh
@@ -49,7 +49,7 @@ colors () {
 #   Enable colors
 # -s/--source [url]
 #   Clone prelude from 'url'.
-#   Defaults to 'https://github.com/bbatsov/prelude.git'
+#   Defaults to 'https://github.com/abizern/prelude.git'
 # -i/--into
 #   If one exists, install into the existing config
 # -n/--no-bytecompile
@@ -66,7 +66,7 @@ usage() {
     printf "  \t \t \t \t If 'dir' is a relative path prefix with $HOME.\n"
     printf "  \t \t \t \t Defaults to $HOME/.emacs.d\n"
     printf "  -s, --source [url] \t \t Clone prelude from 'url'.\n"
-    printf "  \t \t \t \t Defaults to 'https://github.com/bbatsov/prelude.git'.\n"
+    printf "  \t \t \t \t Defaults to 'https://github.com/abizern/prelude.git'.\n"
     printf "  -n, --no-bytecompile \t \t Skip the bytecompilation step of prelude.\n"
     printf "  -i, --into \t \t \t Install Prelude into the existing configuration\n"
     printf "  \t \t \t \t The default behavious is to install prelude into the existing\n"
@@ -118,7 +118,7 @@ done
 
 VERBOSE_COLOR=$BBLUE
 
-[ -z $PRELUDE_URL ] && PRELUDE_URL="https://github.com/bbatsov/prelude.git"
+[ -z $PRELUDE_URL ] && PRELUDE_URL="https://github.com/abizern/prelude.git"
 [ -z "$PRELUDE_INSTALL_DIR" ] && PRELUDE_INSTALL_DIR="$HOME/.emacs.d"
 
 if [ x$PRELUDE_VERBOSE != x ]
