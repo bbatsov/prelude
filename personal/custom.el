2858ef5e59233ae5934d601f6f41eafa9ab24c31
Ross Donaldson <gastove@gmail.com>
2013-08-18 13:53:20 -0700

    Making my own customizations to a fork go bbastov/prelude. Tweaking a few configs, adding packages; switching the theme. Nothing much to see here.

---
+++ personal/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("1f3304214265481c56341bcee387ef1abb684e4efbccebca0e120be7b1a13589" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(prelude-require-package 'twilight-theme)
+(load-theme 'twilight t)
