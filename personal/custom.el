289a2c0c02febbc78e9fa7fd6afed061323153f0
Ross Donaldson <gastove@gmail.com>
2013-08-19 14:54:54 -0700

    Tweaking things to add hyde, autopair mode.

--- personal/custom.el
+++ personal/custom.el
@@ -3,9 +3,34 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
  '(custom-safe-themes
    (quote
-    ("1f3304214265481c56341bcee387ef1abb684e4efbccebca0e120be7b1a13589" default))))
+    ("fc6e906a0e6ead5747ab2e7c5838166f7350b958d82e410257aeeb2820e8a07a" "1f3304214265481c56341bcee387ef1abb684e4efbccebca0e120be7b1a13589" default)))
+ '(fci-rule-color "#383838")
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -13,5 +38,11 @@
  ;; If there is more than one, they won't work right.
  )
 
-(prelude-require-package 'twilight-theme)
+(add-to-list 'load-path "~/.emacs.d/personal/hyde")
+(require 'hyde)
+(prelude-require-packages '(autopair twilight-theme))
 (load-theme 'twilight t)
+
+
+
+;; custom.el ends here
