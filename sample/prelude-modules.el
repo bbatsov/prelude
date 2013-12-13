5e9f19bc8914c7d70f7b8b79595101cae86259d2
Gabriel J. Pérez Irizarry <gabrieljoel@gmail.com>
2013-12-13 09:43:43 -0400

    Adds comments to sample modules.

--- sample/prelude-modules.el
+++ sample/prelude-modules.el
@@ -1,7 +1,7 @@
 ;;; Uncomment the modules you'd like to use and restart Prelude afterwards
 
-(require 'prelude-ido)
-;; (require 'prelude-helm)
+(require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
+;; (require 'prelude-helm) ;; Interface for narrowing and search
 
 (require 'prelude-c)
 ;; (require 'prelude-clojure)
@@ -9,20 +9,20 @@
 ;; (require 'prelude-common-lisp)
 ;; (require 'prelude-css)
 (require 'prelude-emacs-lisp)
-(require 'prelude-erc)
+(require 'prelude-erc) ;; Emacs IRC client
 ;; (require 'prelude-erlang)
 ;; (require 'prelude-haskell)
 (require 'prelude-js)
-;; (require 'prelude-key-chord)
+;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
 ;; (require 'prelude-latex)
 (require 'prelude-lisp)
 ;; (require 'prelude-mediawiki)
-(require 'prelude-org)
+(require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 (require 'prelude-perl)
 ;; (require 'prelude-python)
 ;; (require 'prelude-ruby)
 ;; (require 'prelude-scala)
 (require 'prelude-scheme)
 ;; (require 'prelude-scss)
-;; (require 'prelude-web)
+;; (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
