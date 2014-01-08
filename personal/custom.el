be932273c084cf292abb6054889bd16945b25220
Ross Donaldson <gastove@gmail.com>
2014-01-08 10:08:03 -0800

    Switch to IPython for my python interpreter

--- personal/custom.el
+++ personal/custom.el
@@ -51,8 +51,18 @@
 ;; Load ESS
 (require 'ess-site)
 
-;; Fix Python indentation
-(setq python-indent 4)
+;; Use IPython
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args "\"
+ python-shell-prompt-regexp \"In \\[[0-9]+\\]: \"
+ python-shell-prompt-output-regexp \"Out\\[[0-9]+\\]: \"
+ python-shell-completion-setup-code
+   \"from IPython.core.completerlib import module_completion\"
+ python-shell-completion-module-string-code
+   \"';'.join(module_completion('''%s'''))\n\"
+ python-shell-completion-string-code
+   \"';'.join(get_ipython().Completer.all_completions('''%s'''))\n\")\"\"\"\"\"\"\"\"\"\"\"")
 
 ;; Generate a list of DBs I connect to commonly
 (setq sql-connection-alist
