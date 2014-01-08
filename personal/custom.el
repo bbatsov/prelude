1cd12d9cd7fb086912c5325f7efece13016e23ce
Ross Donaldson <gastove@gmail.com>
2014-01-08 10:30:55 -0800

    Fix bewildering copy-paste errors with IPython setup

--- personal/custom.el
+++ personal/custom.el
@@ -54,15 +54,15 @@
 ;; Use IPython
 (setq
  python-shell-interpreter "ipython"
- python-shell-interpreter-args "\"
- python-shell-prompt-regexp \"In \\[[0-9]+\\]: \"
- python-shell-prompt-output-regexp \"Out\\[[0-9]+\\]: \"
+ python-shell-interpreter-args ""
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
  python-shell-completion-setup-code
-   \"from IPython.core.completerlib import module_completion\"
+ "from IPython.core.completerlib import module_completion"
  python-shell-completion-module-string-code
-   \"';'.join(module_completion('''%s'''))\n\"
+ "';'.join(module_completion('''%s'''))\n"
  python-shell-completion-string-code
-   \"';'.join(get_ipython().Completer.all_completions('''%s'''))\n\")\"\"\"\"\"\"\"\"\"\"\"")
+ "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
 ;; Generate a list of DBs I connect to commonly
 (setq sql-connection-alist
