2383e5730c945c7c7cd3795d8092cd70977eb30d
Bozhidar Batsov <bozhidar@tradeo.com>
2014-04-15 15:51:37 +0300

    Enable desktop-save-mode by default

--- core/prelude-ui.el
+++ core/prelude-ui.el
@@ -75,5 +75,11 @@
 ;; use zenburn as the default theme
 (load-theme 'zenburn t)
 
+(require 'desktop)
+(setq desktop-path (list prelude-savefile-dir))
+(setq desktop-dirname prelude-savefile-dir)
+
+(desktop-save-mode +1)
+
 (provide 'prelude-ui)
 ;;; prelude-ui.el ends here
