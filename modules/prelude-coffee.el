d3954749fa56f4c06d2e3abe2159823914f07f94
Bozhidar Batsov <bozhidar@tradeo.com>
2014-05-15 17:38:15 +0300

    [Fix #535] Correct coffee-mode's config

--- modules/prelude-coffee.el
+++ modules/prelude-coffee.el
@@ -36,32 +36,31 @@
 
 (eval-after-load 'coffee-mode
   '(progn
+     ;; CoffeeScript uses two spaces.
+     (setq coffee-tab-width 2)
+
+     ;; If you don't have js2-mode
+     (setq coffee-js-mode 'javascript-mode)
+
+     ;; If you don't want your compiled files to be wrapped
+     (setq coffee-args-compile '("-c" "--bare"))
+
+     ;; *Messages* spam
+     (setq coffee-debug-mode t)
+
+     ;; Emacs key binding
+     (define-key coffee-mode-map [(meta r)] 'coffee-compile-buffer)
+
+     (setq coffee-command "coffee")
+
      (defun prelude-coffee-mode-defaults ()
        "coffee-mode-defaults"
-
-       ;; CoffeeScript uses two spaces.
-       (setq coffee-tab-width 2)
-
-       ;; If you don't have js2-mode
-       (setq coffee-js-mode 'javascript-mode)
-
-       ;; If you don't want your compiled files to be wrapped
-       (setq coffee-args-compile '("-c" "--bare"))
-
-       ;; *Messages* spam
-       (setq coffee-debug-mode t)
-
-       ;; Emacs key binding
-       (define-key coffee-mode-map [(meta r)] 'coffee-compile-buffer)
-
-       ;; Riding edge.
-       (setq coffee-command "~/dev/coffee")
-
        ;; Compile '.coffee' files on every save
        (and (buffer-file-name)
             (file-exists-p (buffer-file-name))
             (file-exists-p (coffee-compiled-file-name (buffer-file-name)))
-            (coffee-cos-mode t)))
+            (coffee-cos-mode t))
+       (subword-mode +1))
 
      (setq prelude-coffee-mode-hook 'prelude-coffee-mode-defaults)
 
