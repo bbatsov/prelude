81953ad033fa50515dfb60d431dec391a7c0d4b8
Bin Huang <huangbin88@foxmail.com>
2014-05-29 14:43:08 +0800

    Move the prelude tips for key-chord-mode to it's module file.

--- modules/prelude-key-chord.el
+++ modules/prelude-key-chord.el
@@ -43,6 +43,16 @@
 (key-chord-define-global "xx" 'execute-extended-command)
 (key-chord-define-global "yy" 'browse-kill-ring)
 
+(defvar key-chord-tips '("Press <jj> quickly to jump to the beginning of a visible word."
+                         "Press <jl> quickly to jump to a visible line."
+                         "Press <jk> quickly to jump to a visible character."
+                         "Press <JJ> quickly to switch to previous buffer."
+                         "Press <uu> quickly to visualize the undo tree."
+                         "Press <xx> quickly to execute extended command."
+                         "Press <yy> quickly to browse the kill ring."))
+
+(setq prelude-tips (append prelude-tips key-chord-tips))
+
 (key-chord-mode +1)
 
 (provide 'prelude-key-chord)
