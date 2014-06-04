eccd2f316aea0e3986e8dfea9854a15eec39fea8
Dylan Patrick Clark <dylanpatrickclark@gmail.com>
2014-06-04 12:50:02 -0700

    Remap ace-window to M-p.

--- core/prelude-global-keybindings.el
+++ core/prelude-global-keybindings.el
@@ -120,7 +120,7 @@
 (global-set-key (kbd "C-c J") 'ace-jump-buffer)
 (global-set-key (kbd "s->") 'ace-jump-buffer)
 
-(global-set-key [remap other-window] 'ace-window)
+(global-set-key (kbd "M-p") 'ace-window)
 
 (provide 'prelude-global-keybindings)
 
