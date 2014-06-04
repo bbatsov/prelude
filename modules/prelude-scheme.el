4995a8ccd94d2a363a9a901032d877b0de0c82d9
toctan <tianjin.sc@gmail.com>
2014-06-04 17:36:05 +0800

    Add geiser for scheme

--- modules/prelude-scheme.el
+++ modules/prelude-scheme.el
@@ -31,8 +31,17 @@
 ;; Boston, MA 02110-1301, USA.
 
 ;;; Code:
+(prelude-require-package 'geiser)
 
 (require 'prelude-lisp)
+(require 'geiser)
+
+;; geiser replies on a REPL to provide autodoc and completion
+(setq geiser-mode-start-repl-p t)
+
+;; keep the home clean
+(setq geiser-repl-history-filename
+      (expand-file-name "geiser-history" prelude-savefile-dir))
 
 (add-hook 'scheme-mode-hook (lambda () (run-hooks 'prelude-lisp-coding-hook)))
 
