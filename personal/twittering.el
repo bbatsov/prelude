3e9d3f5cb14a901dd4b01ef2f351b38474d6dc9d
Ross Donaldson <gastove@gmail.com>
2014-07-11 17:28:49 -0600

    Try moving some configs to a separate file -- in this case, twittering configs to twittering.el

---
+++ personal/twittering.el
@@ -0,0 +1,15 @@
+;;; twittering.el -- Configs for twittering-mode
+
+;;; Commentary:
+
+;; Configures the behavior of twittering-mode
+
+;;; Code:
+
+(setq twittering-icon-mode t)
+(setq twittering-use-master-password t)
+(setq twittering-use-icon-storage t)
+
+(provide 'twittering)
+
+;;; twittering.el ends here
