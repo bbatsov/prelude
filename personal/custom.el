062bab21d02a73b87e9f879441faa695727962eb
Ross Donaldson <gastove@gmail.com>
2014-07-12 19:05:49 -0600

    Finally get python virtualenvs... something more like usable.

--- personal/custom.el
+++ personal/custom.el
@@ -3,33 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
- '(custom-safe-themes
-   (quote
-    ("fc6e906a0e6ead5747ab2e7c5838166f7350b958d82e410257aeeb2820e8a07a" "1f3304214265481c56341bcee387ef1abb684e4efbccebca0e120be7b1a13589" default)))
+ '(ansi-color-names-vector ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(custom-safe-themes (quote ("fc6e906a0e6ead5747ab2e7c5838166f7350b958d82e410257aeeb2820e8a07a" "1f3304214265481c56341bcee387ef1abb684e4efbccebca0e120be7b1a13589" default)))
  '(fci-rule-color "#383838")
+ '(safe-local-variable-values (quote ((project-venv-name . "mashboard"))))
  '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#BC8383")
-     (40 . "#CC9393")
-     (60 . "#DFAF8F")
-     (80 . "#D0BF8F")
-     (100 . "#E0CF9F")
-     (120 . "#F0DFAF")
-     (140 . "#5F7F5F")
-     (160 . "#7F9F7F")
-     (180 . "#8FB28F")
-     (200 . "#9FC59F")
-     (220 . "#AFD8AF")
-     (240 . "#BFEBBF")
-     (260 . "#93E0E3")
-     (280 . "#6CA0A3")
-     (300 . "#7CB8BB")
-     (320 . "#8CD0D3")
-     (340 . "#94BFF3")
-     (360 . "#DC8CC3"))))
+ '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
  '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -49,18 +28,22 @@
                             company
                             virtualenvwrapper
                             org
-                            protobuf-mode))
+                            protobuf-mode
+                            jedi))
 
-;; Load my Theme of Choice
+;;; Color Theme
 (load-theme 'junio t)
 
+;;; Whitespace Mode
 ;; Disable whitespace-mode in certain other major modes
 (add-hook 'php-mode-hook (lambda() (whitespace-mode -1)))
+(add-hook 'org-mode-hook (lambda() (whitespace-mode -1)))
 
+;;; ESS:
 ;; Load ESS
 (require 'ess-site)
 
-;; Python Configs
+;;; Python Configs
 ;; Use IPython as my python interpreter
 (setq
  python-shell-interpreter "ipython"
@@ -78,15 +61,36 @@
 (venv-initialize-interactive-shells)
 (setq venv-location "~/.python_virtualenvs/")
 
-;; Tweak Mac Keyboard Behavior
+;; Jedi-Mode
+(add-hook 'python-mode-hook (lambda () (company-mode -1)))
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+
+;; Virtualenvs
+(add-hook 'python-mode-hook
+          (lambda ()
+            (hack-local-variables)
+            (when (boundp 'project-venv-name)
+              (venv-workon project-venv-name)
+              (venv-cdvirtualenv))))
+
+(add-hook 'venv-postactivate-hook
+          (lambda ()
+            (let ((env-bin-path (concat (getenv "VIRTUAL_ENV") "bin/postactivate-emacs")))
+              (when (file-exists-p env-bin-path)
+                (load env-bin-path)))))
+
+(setq-default mode-line-format (cons '(:exec venv-current-name) mode-line-format))
+
+;;; Tweak Mac Keyboard Behavior
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
 
-;; Configure projectile
+;;; Projectile
 (setq projectile-remember-window-configs t)
 (setq projectile-switch-project-action 'projectile-dired)
 
-;; Twittering-mode configs
+;;; Twittering-mode
 (setq twittering-icon-mode t)
 (setq twittering-use-master-password t)
 (setq twittering-use-icon-storage t)
@@ -97,10 +101,11 @@
 ;; Smartparens all the time
 (add-hook 'global-init-hook 'smartparens-mode)
 
+;;; ERC
 ;; Setup ERC
 (setq erc-autojoin-channels-alist '(("freenode.net"
-                                     "#emacs" "#clojure" "#julia")))
-
+                                     "#emacs" "#clojure")))
+;; Regular IRC
 (setq erc-nick "gastove")
 (when (file-exists-p (expand-file-name "~/.ercpass"))
   (load "~/.ercpass")
@@ -110,6 +115,7 @@
   (setq erc-nickserv-passwords
         `((freenode ((,erc-nick . ,erc-pass))))))
 
+;; Work IRC
 (defun start-irc-work ()
   "Connect to the UA IRC Server"
   (interactive)
@@ -119,10 +125,9 @@
            :nick "gastove"
            :full-name "Ross Donaldson"
            :password work-erc-pass)
-  (setq erc-autojoin-channels-alist '(("#ops")))
-  )
+  (setq erc-autojoin-channels-alist '(("#ops"))))
 
-;; For SQL hackery -- needs more work
+;;; Work SQL Functions
 ;; Generate a list of DBs I connect to commonly
 (when (file-exists-p (expand-file-name "~/.emacs-dbs"))
   (load "~/.emacs-dbs")
@@ -153,7 +158,8 @@
 (global-set-key (kbd "M-s q") 'sql-connect-preset-by-name) ; Connect to a db preset by nameq
 
 
-;; Support for Marked.app -- assumes you're on a Mac and have Marked.app installed.
+;;; Support for Marked.app -- assumes you're on a Mac,
+;;; and have Marked.app installed.
 (defun markdown-preview-file ()
   "run Marked on the current file and revert the buffer"
   (interactive)
@@ -163,11 +169,13 @@
   )
 (global-set-key (kbd "C-c m") 'markdown-preview-file)
 
+;;; Org Mode
 ;; Support for Babel Mode code blocks
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((python . t)
-   (emacs-lisp .t)))
+   (emacs-lisp .t)
+   (clojure . t)))
 
 ;; Config org export backends
 (setq org-export-backends
