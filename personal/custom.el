320321ea6d97935eaccc5d2b70ab6bc000afcd43
Ross Donaldson <gastove@gmail.com>
2014-07-23 16:44:15 -0700

    Get this like 98 percent of the way working.

--- personal/custom.el
+++ personal/custom.el
@@ -186,18 +186,27 @@
 
 ;;; JavaScripts
 ;; Flycheck for jsl
-(flycheck-define-checker javascript-jsl-checker
-  "A syntax checker for JavaScript based on jsl. See: https://www.npmjs.org/package/jsl
+(eval-after-load "flycheck"
+  '(progn
+     (flycheck-define-checker javascript-jsl-checker
+       "A syntax checker for JavaScript based on jsl. See: https://www.npmjs.org/package/jsl
 
-Requires a local, global install of jsl from npm -- i.e., npm install -g jsl"
-  :command ("/usr/local/bin/jsl" source-inplace)
-  :error-patterns ((error line-start "E" (file-name) "L" line ":" (message) line-end))
-  :modes (js2-mode)
-  :predicate (lambda () (boundp 'use-ua-js)))
+Requires a local, global install of jsl from npm -- i.e., npm install -g jsl.
 
-(add-to-list 'flycheck-checkers 'javavascript-jsl-checker)
+Sample checker output:
+$ jsl index.js
+E ./index.js L56: \"){\n\" should match \") {\"
+1 error
+checked 1; NOT OK
+"
+       :command ("/usr/local/bin/jsl" source-inplace)
+       :error-patterns ((error line-start "E " (file-name) " L" line ": " (message) line-end))
+       :modes (js-mode js2-mode js3-mode)
+       :predicate (lambda () (boundp 'use-ua-js)))
+     (add-to-list 'flycheck-checkers 'javavascript-jsl-checker)
+     (add-hook 'js-mode-hook (lambda () (flycheck-select-checker 'javascript-jsl-checker))))
+  )
 
-(add-hook 'js2-mode-hook (lambda () flycheck-select-checker 'javascript-jsl-checker))
 (provide 'custom)
 
 ;;; custom.el ends here
