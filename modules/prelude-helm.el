8f028ffb89e0b06ae8144ad8826bdac5167cbd10
toctan <tianjin.sc@gmail.com>
2014-09-04 22:39:40 +0800

    Remove unnecessary helm configuration code
    There options are already the default.

--- modules/prelude-helm.el
+++ modules/prelude-helm.el
@@ -57,15 +57,12 @@
  helm-google-suggest-use-curl-p t
  helm-scroll-amount 4 ; scroll 4 lines other window using M-<next>/M-<prior>
  helm-quick-update t ; do not display invisible candidates
- helm-idle-delay 0.01 ; be idle for this many seconds, before updating in delayed sources.
- helm-input-idle-delay 0.01 ; be idle for this many seconds, before updating candidate buffer
  helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
  helm-split-window-default-side 'other ;; open helm buffer in another window
  helm-split-window-in-side-p t ;; open helm buffer inside current window, not occupy whole other window
  helm-buffers-favorite-modes (append helm-buffers-favorite-modes
                                      '(picture-mode artist-mode))
  helm-candidate-number-limit 500 ; limit the number of displayed canidates
- helm-M-x-requires-pattern 0    ; show all candidates when set to 0
  helm-ff-file-name-history-use-recentf t
  helm-move-to-line-cycle-in-source t ; move to end or beginning of source
                                         ; when reaching top or bottom of source.
@@ -78,9 +75,6 @@
 (global-set-key (kbd "C-c h C-c w") 'helm-wikipedia-suggest)
 (global-set-key (kbd "C-c h x") 'helm-register)
 (global-set-key (kbd "C-c h SPC") 'helm-all-mark-rings)
-
-;;; Save current position to mark ring
-(add-hook 'helm-goto-line-before-hook 'helm-save-current-pos-to-mark-ring)
 
 ;; PACKAGE: helm-projectile
 (require 'helm-projectile)
