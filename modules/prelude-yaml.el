fca390003c5c48b836abd3b94c857f63a3cf3308
Bozhidar Batsov <bozhidar@tradeo.com>
2014-09-10 17:56:00 +0300

    Enable subword-mode for yaml-mode

--- modules/prelude-yaml.el
+++ modules/prelude-yaml.el
@@ -36,6 +36,7 @@
 ;; yaml-mode doesn't derive from prog-mode, but we can at least enable
 ;; whitespace-mode and apply cleanup.
 (add-hook 'yaml-mode-hook 'whitespace-mode)
+(add-hook 'yaml-mode-hook 'subword-mode)
 (add-hook 'yaml-mode-hook
           (lambda () (add-hook 'before-save-hook 'prelude-cleanup-maybe nil t)))
 
