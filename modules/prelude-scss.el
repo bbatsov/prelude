cd29262c36946f15fd0fd8ce1961079b43873db7
Bozhidar Batsov <bozhidar@tradeo.com>
2014-09-18 18:07:25 +0300

    Don't set scss-compile-at-save in the mode hook

--- modules/prelude-scss.el
+++ modules/prelude-scss.el
@@ -35,10 +35,11 @@
 (require 'prelude-css)
 (prelude-require-packages '(scss-mode))
 
+;; turn off annoying auto-compile on save
+(setq scss-compile-at-save nil)
+
 (defun prelude-scss-mode-defaults ()
-  (prelude-css-mode-defaults)
-  ;; turn off annoying auto-compile on save
-  (setq scss-compile-at-save nil))
+  (prelude-css-mode-defaults))
 
 (setq prelude-scss-mode-hook 'prelude-scss-mode-defaults)
 
