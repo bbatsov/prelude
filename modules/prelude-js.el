0da2bd461cde935f5507db909a97b455dee47ddd
toctan <tianjin.sc@gmail.com>
2014-09-26 10:28:07 +0800

    remove curly braces from js electric-layout-rules
    fix #694

--- modules/prelude-js.el
+++ modules/prelude-js.el
@@ -37,8 +37,8 @@
 (eval-after-load 'js-mode
   '(progn
      (defun prelude-js-mode-defaults ()
-       ;; electric-layout-mode doesn't play nice with js-mode
-       (electric-layout-mode -1))
+       ;; electric-layout-mode doesn't play nice with smartparens
+       (setq-local electric-layout-rules '((?\; . after))))
 
      (setq prelude-js-mode-hook 'prelude-js-mode-defaults)
 
