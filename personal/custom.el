b1f25a6b41b7374fa0f03a94612b60f6d5d9c19a
Ross Donaldson <gastove@gmail.com>
2014-11-05 09:34:39 -0800

    WAT

--- personal/custom.el
+++ personal/custom.el
@@ -112,11 +112,11 @@
 
 ;;; Malabar Mode for the Jabbas
  ;;; Malabar Mode for the Jabbas
-+(require 'cedet)
-+(require 'semantic)
-+(load "semantic/loaddefs.el")
-+(semantic-mode 1);;
-+(require 'malabar-mode)
+(require 'cedet)
+(require 'semantic)
+(load "semantic/loaddefs.el")
+(semantic-mode 1);;
+(require 'malabar-mode)
 (add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
 
 ;;; Org Mode
