8c1b81ea708c43d7c284b0e8119ab587a63652f2
Ross Donaldson <gastove@gmail.com>
2014-11-05 12:35:22 -0800

    Tweak helm appearance

--- personal/custom.el
+++ personal/custom.el
@@ -15,7 +15,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(helm-selection ((t (:background "gray10")))))
 
 ;; Pull in custom packages
 (prelude-require-packages '(jade-mode
