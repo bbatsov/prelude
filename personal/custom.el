dea289dac133455406a24f1113bceffc2a094032
Ross Donaldson <gastove@gmail.com>
2014-11-05 22:36:05 -0800

    Update helm colors

--- personal/custom.el
+++ personal/custom.el
@@ -15,7 +15,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(helm-selection ((t (:background "gray10")))))
+ '(helm-ff-directory ((t (:background "gray0" :foreground "DarkRed"))))
+ '(helm-selection ((t (:background "gray14")))))
 
 ;; Pull in custom packages
 (prelude-require-packages '(jade-mode
