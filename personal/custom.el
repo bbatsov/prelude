c7aeb29101bd681ad2029f5feae721c862f583c1
Ross Donaldson <gastove@gmail.com>
2014-11-07 21:23:33 -0800

    Extract ESS configs to their own file.

--- personal/custom.el
+++ personal/custom.el
@@ -66,6 +66,7 @@
                                   "/work.el"            ;; Contains work erc configs too.
                                   "/python-configs.el"
                                   "/erc-configs.el"
+                                  "/ess-configs.el"
                                   "/jsx-configs.el"
                                   "/web-mode-configs.el"))
 
@@ -81,23 +82,6 @@
 (add-hook 'org-mode-hook (lambda() (whitespace-mode -1)))
 (add-hook 'markdown-mode-hook (lambda () (whitespace-mode -1)))
 
-;;; ESS:
-;; Load ESS
-(require 'ess-site)
-
-(add-to-list 'ess-style-alist
-             '(my-RRR (ess-indent-level . 2)
-                      (ess-first-continued-statement-offset . 2)
-                      ;; (ess-first-continued-statement-offset . 0)
-                      (ess-continued-statement-offset . 0)
-                      ;; (ess-continued-statement-offset . 4)
-                      (ess-brace-offset . 0)
-                      (ess-arg-function-offset . 4)
-                      (ess-arg-function-offset-new-line . '(4))
-                      (ess-expression-offset . 4)
-                      (ess-else-offset . 0)
-                      (ess-close-brace-offset . 0)))
-(setq ess-default-style 'my-RRR)
 
 
 ;;; Tweak Mac Keyboard Behavior
