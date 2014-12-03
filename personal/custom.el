d3f3ccba061dd6c31d865a556ebd854a39f624e5
Ross Donaldson <gastove@gmail.com>
2014-12-03 10:25:56 -0800

    Unpoot

--- personal/custom.el
+++ personal/custom.el
@@ -46,8 +46,6 @@
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
-;; poot
-
 ;; Pull in custom packages
 (prelude-require-packages '(jade-mode
                             ess
