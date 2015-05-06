195e12d7b4ad3bd8d83ce6b7450b8d0fe6b8d399
Charles Lowell <cowboyd@frontside.io>
2015-05-06 09:17:56 -0500

    always split horizontally.
    It seems to be the right thing to do almost always. This also splits out
    anytihng to do with windowing commands into its own module.

--- frontside/frontside.el
+++ frontside/frontside.el
@@ -15,7 +15,6 @@
 
 ;;(disable-theme 'zenburn)
 (prelude-require-packages '(frame-fns frame-cmds))
-(global-set-key (kbd "<s-return>") 'toggle-frame-maximized)
 
 ;; YAS snippets everywhere bro.
 (prelude-require-package 'yasnippet)
