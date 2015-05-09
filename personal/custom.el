ef53e43eed5f29586a1345f3ef0aefe37d9cccad
Ross Donaldson <gastove@gmail.com>
2015-05-09 09:32:38 -0700

    Funcs

--- personal/custom.el
+++ personal/custom.el
@@ -92,7 +92,8 @@
                                   "/jsx-configs.el"
                                   "/jabber-configs.el"
                                   "/web-mode-configs.el"
-                                  "/flip-tables.el"))
+                                  "/flip-tables.el"
+                                  "/custom-fns.el"))
 
 (mapc (lambda (rmd-file-name)
         (load (concat prelude-personal-dir rmd-file-name)))
@@ -158,15 +159,15 @@
                           (company-mode)))
 
 
-;; COLORS
-(require 'color)
-(let ((bg (face-attribute 'default :background)))
-  (custom-set-faces
-   `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
-   `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
-   `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
-   `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
-   `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
+;; Disabling this; I think I've done this better in Rhombus
+;; (require 'color)
+;; (let ((bg (face-attribute 'default :background)))
+;;   (custom-set-faces
+;;    `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+;;    `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+;;    `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+;;    `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+;;    `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
 
 ;; Not convinced this is helping.
 (setq company-idle-delay .4)
@@ -226,40 +227,6 @@
 ;;; Polymode for markdown
 (add-to-list 'auto-mode-alist '("\\.md" . poly-markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown" . poly-markdown-mode))
-
-;;; A handy-dandy function for rotating windows
-(defun toggle-window-split ()
-  (interactive)
-  (if (= (count-windows) 2)
-      (let* ((this-win-buffer (window-buffer))
-             (next-win-buffer (window-buffer (next-window)))
-             (this-win-edges (window-edges (selected-window)))
-             (next-win-edges (window-edges (next-window)))
-             (this-win-2nd (not (and (<= (car this-win-edges)
-                                         (car next-win-edges))
-                                     (<= (cadr this-win-edges)
-                                         (cadr next-win-edges)))))
-             (splitter
-              (if (= (car this-win-edges)
-                     (car (window-edges (next-window))))
-                  'split-window-horizontally
-                'split-window-vertically)))
-        (delete-other-windows)
-        (let ((first-win (selected-window)))
-          (funcall splitter)
-          (if this-win-2nd (other-window 1))
-          (set-window-buffer (selected-window) this-win-buffer)
-          (set-window-buffer (next-window) next-win-buffer)
-          (select-window first-win)
-          (if this-win-2nd (other-window 1))))))
-
-(global-set-key (kbd "C-x |") 'toggle-window-split)
-
-;;; Put in a nicely formatted Today
-(defun insert-iso-date ()
-  (interactive)
-  (insert (format-time-string "%Y-%m-%d" (current-time))))
-(global-set-key (kbd "C-x j") 'insert-iso-date)
 
 (provide 'custom)
 
