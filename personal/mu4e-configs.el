664778557fdeb024d3734bda0895ee3e83de05b0
Ross Donaldson <gastove@gmail.com>
2015-05-11 22:24:12 -0700

    Support multiple email accounts :D

--- personal/mu4e-configs.el
+++ personal/mu4e-configs.el
@@ -92,6 +92,42 @@
       smtpmail-smtp-server "smtp.gmail.com"
       smtpmail-smtp-service 587)
 
+;; Tweak bookmarked querries
+(add-to-list 'mu4e-bookmarks '("flag:unread AND date:today..now" "Today's Unreads" ?h))
+
+;; Switch accounts
+
+(defvar my-mu4e-account-alist
+  '(("gastove@gmail.com"
+     (mu4e-drafts-folder "/gastove@gmail.com/[Gmail].Drafts")
+     (mu4e-sent-folder   "/gastove@gmail.com/[Gmail].Sent Mail")
+     (user-mail-address "gastove@gmail.com"))
+    ("ross@urbanairship.com"
+     (mu4e-drafts-folder "/ross@urbanairship.com/[Gmail].Drafts")
+     (mu4e-sent-folder   "/ross@urbanairship.com/[Gmail].Sent Mail")
+     (user-mail-address "ross@urbanairship.com"))))
+
+(defun my-mu4e-set-account ()
+  "Set the account for composing a message."
+  (let* ((account
+          (if mu4e-compose-parent-message
+              (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+                (string-match "/\\(.*?\\)/" maildir)
+                (match-string 1 maildir))
+            (completing-read (format "Compose with account: (%s) "
+                                     (mapconcat #'(lambda (var) (car var))
+                                                my-mu4e-account-alist "/"))
+                             (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
+                             nil t nil nil (caar my-mu4e-account-alist))))
+         (account-vars (cdr (assoc account my-mu4e-account-alist))))
+    (if account-vars
+        (mapc #'(lambda (var)
+                  (set (car var) (cadr var)))
+              account-vars)
+      (error "No email account found"))))
+
+(add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
+
 (provide 'mu4e-configs)
 
 ;;; mu4e-configs.el ends here
