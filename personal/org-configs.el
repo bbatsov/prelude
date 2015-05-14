bff44bfd5419f59b07872a1dfc2e3cba8a068237
Ross Donaldson <gastove@gmail.com>
2015-05-14 08:22:36 -0700

    Tweak org mode to support more TODO keywords

--- personal/org-configs.el
+++ personal/org-configs.el
@@ -3,6 +3,20 @@
 ;; Make sure auto-fill-mode is on. Pretty much always need it.
 (add-hook 'org-mode-hook (lambda () (turn-on-auto-fill)))
 
+;; TODO Keyword states:
+;; > In-Progress states: TODO, DOING, BLOCKED
+;; > Finished states:    DONE, IMPOSSIBLE, CANCELLED
+(setq org-todo-keywords
+      '((type "TODO" "DOING" "BLOCKED" "|" "CANCELLED" "IMPOSSIBLE" "DONE")))
+
+(setq org-todo-keyword-faces
+     '(("TODO" . org-todo)
+       ("DOING" . org-todo)
+       ("BLOCKED" . org-warning)
+       ("CANCELLED" . org-done)
+       ("IMPOSSIBLE" . org-done)
+       ("DONE" . org-done)))
+
 ;; Support for Babel Mode code blocks
 ;; NOTE: requires the addition of the org elpa repo!
 
