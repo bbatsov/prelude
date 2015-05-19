42ab34ce97ff1c851e49b9b4e018b975c0a76eff
Ross Donaldson <gastove@gmail.com>
2015-05-18 17:12:14 -0700

    Clear out hard-coded Jabber colors

--- personal/custom.el
+++ personal/custom.el
@@ -38,8 +38,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(jabber-chat-prompt-foreign ((t (:foreground "steel blue" :weight bold))))
- '(jabber-chat-prompt-local ((t (:foreground "light gray" :weight bold)))))
+ )
 
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
