e4694ad821d775043a701e7c0a8fba2d7ce50213
Charles Lowell <cowboyd@frontside.io>
2015-05-22 10:41:22 -0500

    dynamically recalculate split-width-threshold
    with every resize, change the split-width threshold

--- frontside/windowing.el
+++ frontside/windowing.el
@@ -5,3 +5,17 @@
 ;; Split horizontally when opening a new window from a command
 ;; whenever possible.
 (setq split-height-threshold nil)
+
+(defun frontside-windowing-adjust-split-width-threshold ()
+  "Change the value of `split-width-threshold' so that it will cause the screen
+split once and only once.
+
+For example, if the frame is 360 columns wide, then we want the
+split-width-threshold to be 181. That way, when you split horizontally, the two
+new windows will each be 180 columns wide, and sit just below the threshold.
+"
+  (setq split-width-threshold (+ 1 (/ (frame-width) 2))))
+
+;; recaculate split-width-threshold with every change
+(add-hook 'window-configuration-change-hook
+ 'frontside-windowing-adjust-split-width-threshold)
