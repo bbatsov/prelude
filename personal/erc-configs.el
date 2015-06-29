2bf68dff52108f5957716768bb38a34949d4079d
Ross Donaldson <gastove@gmail.com>
2015-06-28 21:52:28 -0700

    Remove old files

--- personal/erc-configs.el
+++
@@ -1,9 +0,0 @@
-;;; Code:
-;; Setup ERC
-(setq erc-autojoin-channels-alist '(("freenode.net"
-                                     "#emacs" "#clojure"))
-      erc-nick "gastove")
-
-(provide 'erc-configs)
-
-;;; erc-configs.el ends here
