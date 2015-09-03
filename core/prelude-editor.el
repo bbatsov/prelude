206b0b76e7b58c37868a0a3974f6332b01837940
toctan <tianjin.sc@gmail.com>
2015-09-03 12:46:50 +0800

    [Fix #899] Make the kill-region advice play nice with rectangle-mark-mode

--- core/prelude-editor.el
+++ core/prelude-editor.el
@@ -197,7 +197,7 @@ The body of the advice is in BODY."
 (defadvice kill-region (before smart-cut activate compile)
   "When called interactively with no active region, kill a single line instead."
   (interactive
-   (if mark-active (list (region-beginning) (region-end))
+   (if mark-active (list (region-beginning) (region-end) rectangle-mark-mode)
      (list (line-beginning-position)
            (line-beginning-position 2)))))
 
