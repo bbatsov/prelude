ba8d2a63683770430827dee05c62f177a33a96de
lvh <_@lvh.io>
2015-10-12 13:13:45 +0200

    Add emoji support
    Enable emoji, and stop the UI from freezing when trying to display them.

--- core/prelude-osx.el
+++ core/prelude-osx.el
@@ -66,5 +66,8 @@ Windows external keyboard from time to time."
 
 (menu-bar-mode +1)
 
+;; Enable emoji, and stop the UI from freezing when trying to display them.
+(set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend)
+
 (provide 'prelude-osx)
 ;;; prelude-osx.el ends here
