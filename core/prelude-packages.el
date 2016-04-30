0447086cc26f8fc651a3101360e4525a3adafb01
Bozhidar Batsov <bozhidar@batsov.com>
2016-04-29 18:01:59 -0700

    Auto-install cask-mode when needed

--- core/prelude-packages.el
+++ core/prelude-packages.el
@@ -136,6 +136,7 @@ PACKAGE is installed only if not already present.  The file is opened in MODE."
     ("\\.coffee\\'" coffee-mode coffee-mode)
     ("\\.css\\'" css-mode css-mode)
     ("\\.csv\\'" csv-mode csv-mode)
+    ("Cask" cask-mode cask-mode)
     ("\\.d\\'" d-mode d-mode)
     ("\\.dart\\'" dart-mode dart-mode)
     ("\\.elm\\'" elm-mode elm-mode)
