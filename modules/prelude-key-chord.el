61dfff4b9ee00cba9324a74ef376d7debaaad9e7
wscisme <wscisme@gmail.com>
2016-05-01 19:01:42 -0700

    Change function name to crux (#992)

--- modules/prelude-key-chord.el
+++ modules/prelude-key-chord.el
@@ -38,7 +38,7 @@
 (key-chord-define-global "jj" 'avy-goto-word-1)
 (key-chord-define-global "jl" 'avy-goto-line)
 (key-chord-define-global "jk" 'avy-goto-char)
-(key-chord-define-global "JJ" 'prelude-switch-to-previous-buffer)
+(key-chord-define-global "JJ" 'crux-switch-to-previous-buffer)
 (key-chord-define-global "uu" 'undo-tree-visualize)
 (key-chord-define-global "xx" 'execute-extended-command)
 (key-chord-define-global "yy" 'browse-kill-ring)
