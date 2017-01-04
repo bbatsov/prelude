e0a86fcb02504b86a9f1fbe35bdbd837a47bd63e
Jennifer Page <jenipage1989@gmail.com>
2017-01-04 02:17:48 -0500

    [Fix #743] Add support for package pinning (#1048)

--- core/prelude-packages.el
+++ core/prelude-packages.el
@@ -43,6 +43,11 @@
   (add-to-list 'package-archives
                '("melpa" . "https://melpa.org/packages/") t))
 
+;; load the pinned packages
+(let ((prelude-pinned-packages-file (expand-file-name "prelude-pinned-packages.el" prelude-dir)))
+  (if (file-exists-p prelude-pinned-packages-file)
+      (load prelude-pinned-packages-file)))
+
 ;; set package-user-dir to be relative to Prelude install path
 (setq package-user-dir (expand-file-name "elpa" prelude-dir))
 (package-initialize)
