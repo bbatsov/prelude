509de86acbdc133ba89a2d6e8d6f790c10e93fc7
Angelo Trivelli <angelotrivelli@gmail.com>
2018-01-29 08:59:00 -0500

    more personalization

--- personal/custom.el
+++ personal/custom.el
@@ -3,12 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
  '(column-number-mode t)
  '(package-selected-packages
    (quote
-    (ess multiple-cursors zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters projectile ov operate-on-number move-text magit json-mode js2-mode imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck expand-region elisp-slime-nav editorconfig easy-kill discover-my-major diminish diff-hl d-mode crux counsel company-anaconda cider browse-kill-ring beacon anzu ace-window)))
- '(size-indication-mode t))
+    (markdown-mode ess multiple-cursors zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters projectile ov operate-on-number move-text magit json-mode js2-mode imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck expand-region elisp-slime-nav editorconfig easy-kill discover-my-major diminish diff-hl d-mode crux counsel company-anaconda cider browse-kill-ring beacon anzu ace-window)))
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
