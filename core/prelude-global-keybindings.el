d6786e33f1bd52ff37af9c826be7db0c70c96b42
Bozhidar Batsov <bozhidar@batsov.com>
2018-05-06 09:35:46 +0300

    [Fix #1147] Add an extra keybinding for ace-window that's Windows friendly

--- core/prelude-global-keybindings.el
+++ core/prelude-global-keybindings.el
@@ -109,7 +109,10 @@
 
 (global-set-key (kbd "C-c j") 'avy-goto-word-or-subword-1)
 (global-set-key (kbd "s-.") 'avy-goto-word-or-subword-1)
+
+;; improved window navigation with ace-window
 (global-set-key (kbd "s-w") 'ace-window)
+(global-set-key [remap other-window] 'ace-window)
 
 (provide 'prelude-global-keybindings)
 
