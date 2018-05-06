270ef647a14801a6900add27d794b7e599a8842b
Bozhidar Batsov <bozhidar@batsov.com>
2018-05-06 09:53:24 +0300

    Improve the code style

--- core/prelude-macos.el
+++ core/prelude-macos.el
@@ -56,10 +56,9 @@ Windows external keyboard from time to time."
         (setq mac-command-modifier 'meta)
         (setq mac-option-modifier 'super)
         (message "Command is now bound to META and Option is bound to SUPER."))
-    (progn
-      (setq mac-command-modifier 'super)
-      (setq mac-option-modifier 'meta)
-      (message "Command is now bound to SUPER and Option is bound to META."))))
+    (setq mac-command-modifier 'super)
+    (setq mac-option-modifier 'meta)
+    (message "Command is now bound to SUPER and Option is bound to META.")))
 
 (define-key prelude-mode-map (kbd "C-c w") 'prelude-swap-meta-and-super)
 (define-key prelude-mode-map (kbd "s-/") 'hippie-expand)
@@ -67,8 +66,8 @@ Windows external keyboard from time to time."
 (menu-bar-mode +1)
 
 ;; Enable emoji, and stop the UI from freezing when trying to display them.
-(if (fboundp 'set-fontset-font)
-    (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend))
+(when (fboundp 'set-fontset-font)
+  (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend))
 
 
 (provide 'prelude-macos)
