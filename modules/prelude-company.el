c142100e631c11f264429d0ab3286d7f72900675
Bozhidar Batsov <bozhidar@batsov.com>
2018-06-26 08:31:25 +0300

    Align company's tooltip annotations to the right

--- modules/prelude-company.el
+++ modules/prelude-company.el
@@ -39,6 +39,7 @@
 (setq company-show-numbers t)
 (setq company-tooltip-limit 10)
 (setq company-minimum-prefix-length 2)
+(setq company-tooltip-align-annotations t)
 ;; invert the navigation direction if the the completion popup-isearch-match
 ;; is displayed on top (happens near the bottom of windows)
 (setq company-tooltip-flip-when-above t)
