f713f0de8f8dc62c5f5637e4f33adb1c34a61d39
LEE Dongjun <redongjun@gmail.com>
2018-06-26 13:23:13 +0900

    Add Typescript programming support.

--- sample/prelude-modules.el
+++ sample/prelude-modules.el
@@ -34,6 +34,7 @@
 (require 'prelude-scheme)
 (require 'prelude-shell)
 ;; (require 'prelude-scss)
+;; (require 'prelude-ts)
 ;; (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
 ;; (require 'prelude-yaml)
