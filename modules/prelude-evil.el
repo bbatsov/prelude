c9177753e73a9d1036f2283d171472618399981b
Henrik Thuesen <henrikthuesen@gmail.com>
2019-04-30 21:23:49 +0200

    Replaces the use of `magit-log-popup' with `magit-log'.
    This is also mentioned in bbatsov/prelude#1226 and originates from
    magit/magit#3695.

--- modules/prelude-evil.el
+++ modules/prelude-evil.el
@@ -114,10 +114,10 @@
 (evil-add-hjkl-bindings magit-commit-mode-map 'emacs)
 (evil-add-hjkl-bindings magit-branch-manager-mode-map 'emacs
   "K" 'magit-discard
-  "L" 'magit-log-popup)
+  "L" 'magit-log)
 (evil-add-hjkl-bindings magit-status-mode-map 'emacs
   "K" 'magit-discard
-  "l" 'magit-log-popup
+  "l" 'magit-log
   "h" 'magit-diff-toggle-refine-hunk)
 
 (setq evil-shift-width 2)
