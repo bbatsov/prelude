c4c839a6d7f66c2e8987d6065d0f4ab220037a4b
azzamsa <azzam@azzamsa.com>
2019-05-04 13:37:58 +0700

    Replace usage of `magit-dispatch-popup` with `magit-dispatch`
    `magit-dispatch-popup` is an obsolete command (as of Magit 2.91.0)

--- core/prelude-global-keybindings.el
+++ core/prelude-global-keybindings.el
@@ -103,7 +103,7 @@
 (global-set-key (kbd "<f12>") 'menu-bar-mode)
 
 (global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch)
 
 (global-set-key (kbd "C-=") 'er/expand-region)
 
