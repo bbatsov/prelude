b2ec31b741415c8f13ab218d0a3c5dc951b7e448
Rohan Hart <rohan.hart@fronde.com>
2020-04-08 10:33:52 +1200

    allow autoformatting of files to be controlled

--- modules/prelude-ts.el
+++ modules/prelude-ts.el
@@ -48,7 +48,10 @@
     (tide-hl-identifier-mode +1))
 
   ;; formats the buffer before saving
-  (add-hook 'before-save-hook 'tide-format-before-save)
+  (add-hook 'before-save-hook
+            (lambda ()
+              (when prelude-format-on-save
+                (tide-format-before-save))))
 
   (setq prelude-ts-mode-hook 'prelude-ts-mode-defaults)
 
