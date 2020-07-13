3169bad45462ccd0f31db42231feeca846230df2
srameshwar <rsundare@gmail.com>
2020-07-13 13:12:44 -0400

    Get the default prelude modiles in as per install instructions

---
+++ personal/prelude-modules.el
@@ -0,0 +1,41 @@
+;;; Uncomment the modules you'd like to use and restart Prelude afterwards
+
+;; Emacs IRC client
+(require 'prelude-erc)
+;; (require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
+(require 'prelude-ivy) ;; A mighty modern alternative to ido
+;; (require 'prelude-helm) ;; Interface for narrowing and search
+;; (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
+(require 'prelude-company)
+;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
+;; (require 'prelude-evil)
+
+;;; Programming languages support
+(require 'prelude-c)
+;; (require 'prelude-clojure)
+;; (require 'prelude-coffee)
+;; (require 'prelude-common-lisp)
+;; (require 'prelude-css)
+(require 'prelude-emacs-lisp)
+;; (require 'prelude-erlang)
+;; (require 'prelude-elixir)
+;; (require 'prelude-go)
+;; (require 'prelude-haskell)
+(require 'prelude-js)
+;; (require 'prelude-latex)
+(require 'prelude-lisp)
+(require 'prelude-lsp)
+;; (require 'prelude-ocaml)
+(require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
+(require 'prelude-perl)
+;; (require 'prelude-python)
+;; (require 'prelude-ruby)
+;; (require 'prelude-rust)
+;; (require 'prelude-scala)
+(require 'prelude-scheme)
+(require 'prelude-shell)
+;; (require 'prelude-scss)
+;; (require 'prelude-ts)
+;; (require 'prelude-web) ;; Emacs mode for web templates
+(require 'prelude-xml)
+;; (require 'prelude-yaml)
