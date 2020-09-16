091c3e53a78d96afa0c0fe5b528b5f9427e1ffe6
Bozhidar Batsov <bozhidar@batsov.com>
2020-09-16 10:46:00 +0300

    Update required Emacs version check

--- utils/installer.sh
+++ utils/installer.sh
@@ -175,9 +175,9 @@ fi
 
 ### Check emacs version
 emacs_version="$(emacs --version 2>/dev/null | sed -n 's/.*[^0-9.]\([0-9]*\.[0-9.]*\).*/\1/p;q' | sed 's/\..*//g')"
-if [ "${emacs_version:-0}" -lt 24 ]
+if [ "${emacs_version:-0}" -lt 25 ]
 then
-    printf "$YELLOW WARNING:$RESET Prelude depends on emacs $RED 24$RESET !\n"
+    printf "$YELLOW WARNING:$RESET Prelude requires Emacs $RED 25$RESET or newer!\n"
 fi
 
 if [ -f "$HOME/.emacs" ]
