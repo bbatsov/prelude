5850c334407796cebb0c85abaf4e9f10dfc429cd
Bozhidar Batsov <bozhidar@batsov.com>
2020-09-20 23:00:54 +0300

    Disable the scheme module in the sample module config
    Scheme is not that popular and the module depends on third-party
    packages.

--- sample/prelude-modules.el
+++ sample/prelude-modules.el
@@ -36,7 +36,7 @@
 ;; (require 'prelude-ruby)
 ;; (require 'prelude-rust)
 ;; (require 'prelude-scala)
-(require 'prelude-scheme)
+;; (require 'prelude-scheme)
 (require 'prelude-shell)
 ;; (require 'prelude-scss)
 ;; (require 'prelude-ts)
