fe5c81ce31bf46281d2dadf7342bbaf9cb2270ee
Bozhidar Batsov <bozhidar@batsov.com>
2020-09-20 10:15:47 +0300

    Restructure a bit the sample modules files

--- sample/prelude-modules.el
+++ sample/prelude-modules.el
@@ -1,8 +1,8 @@
 ;;; Uncomment the modules you'd like to use and restart Prelude afterwards
 
-;; Emacs IRC client
-(require 'prelude-erc)
-;; (require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
+;;; General productivity tools
+
+;; (require 'prelude-ido) ;; Supercharges Emacs completion for C-x C-f and more
 (require 'prelude-ivy) ;; A mighty modern alternative to ido
 ;; (require 'prelude-helm) ;; Interface for narrowing and search
 ;; (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
@@ -10,7 +10,11 @@
 ;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
 ;; (require 'prelude-evil)
 
+;;; Org-mode (a legendary productivity tool that deserves its own category)
+(require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
+
 ;;; Programming languages support
+
 (require 'prelude-c)
 ;; (require 'prelude-clojure)
 ;; (require 'prelude-coffee)
@@ -27,7 +31,6 @@
 (require 'prelude-lisp)
 (require 'prelude-lsp)
 ;; (require 'prelude-ocaml)
-(require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 (require 'prelude-perl)
 ;; (require 'prelude-python)
 ;; (require 'prelude-ruby)
@@ -40,3 +43,6 @@
 ;; (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
 ;; (require 'prelude-yaml)
+
+;;; Misc
+(require 'prelude-erc) ;; A popular Emacs IRC client (useful if you're still into Freenode)
