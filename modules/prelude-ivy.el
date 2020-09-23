eb1702eed7af9cb51d55d5f18ee2aa79185ef7c7
Bozhidar Batsov <bozhidar@batsov.com>
2020-09-23 09:53:33 +0300

    Tweak the ivy module

--- modules/prelude-ivy.el
+++ modules/prelude-ivy.el
@@ -9,7 +9,10 @@
 
 ;;; Commentary:
 
-;; Ivy-related config.
+;; Ivy-related config.  Ivy is a smart framework for minibuffer
+;; completion/filtering/selection (think of ido).  Swiper and counsel
+;; are two packages built on top of ivy that provide ivy-powered
+;; versions of popular Emacs commands.
 
 ;;; License:
 
@@ -31,6 +34,7 @@
 ;;; Code:
 (prelude-require-packages '(ivy swiper counsel))
 
+;;; Ivy
 ;; ivy is a powerful alternative to the popular ido-mode
 
 (require 'ivy)
@@ -43,11 +47,12 @@
 
 (setq projectile-completion-system 'ivy)
 
+;;; Swiper
 ;; swiper provides enhanced buffer search
 
 (global-set-key "\C-s" 'swiper)
 
-
+;;; Counsel
 ;; counsel supercharges a lot of commands with some ivy magic
 
 (global-set-key (kbd "M-x") 'counsel-M-x)
@@ -62,7 +67,6 @@
 (global-set-key (kbd "C-c a") 'counsel-ag)
 (global-set-key (kbd "C-x l") 'counsel-locate)
 (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
-
 
 (provide 'prelude-ivy)
 ;;; prelude-ivy.el ends here
