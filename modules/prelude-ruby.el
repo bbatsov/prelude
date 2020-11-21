705ac3b9ccd19d6512bd2597880284ee4aba1b78
Bozhidar Batsov <bozhidar@batsov.com>
2020-11-21 13:45:34 +0200

    Don't auto-insert coding comments in Ruby

--- modules/prelude-ruby.el
+++ modules/prelude-ruby.el
@@ -37,10 +37,14 @@
 ;; We never want to edit Rubinius bytecode
 (add-to-list 'completion-ignored-extensions ".rbc")
 
+;; Map yari to C-h R
 (define-key 'help-command (kbd "R") 'yari)
 
 (with-eval-after-load 'ruby-mode
   (defun prelude-ruby-mode-defaults ()
+    ;; Don't auto-insert encoding comments
+    ;; Those are almost never needed in Ruby 2+
+    (setq ruby-insert-encoding-magic-comment nil)
     (inf-ruby-minor-mode +1)
     ;; CamelCase aware editing operations
     (subword-mode +1))
