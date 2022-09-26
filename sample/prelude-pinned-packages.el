8197c73b14a341a8b8a6f8cd620466d566ebdb2d
Ron Parker <rdparker@gmail.com>
2022-09-26 16:37:04 -0400

    Add magit-section to pinned packages
    This addresses the
    
      which-func-ff-hook error: (void-function magit-section--backward-find)
    
    seen when `magit-status' is run in a buffer with imenu support on a
    fresh Prelude checkout using the sample `prelude-pinned-packages.el'.
    
    In magit 3.3.0, magit-section was spun off into its own package.
    Later, magit commit 4727dcb removed the `magit-section--backward-find'
    from magit-section.el.  Therefore, unless magit and magit-section are
    in the same pinned or unpinned state the above error will occur.

--- sample/prelude-pinned-packages.el
+++ sample/prelude-pinned-packages.el
@@ -83,6 +83,7 @@
         (macrostep . "melpa-stable")
         (magit . "melpa-stable")
         (magit-popup . "melpa-stable")
+        (magit-section . "melpa-stable")
         (makey . "melpa-stable")
         (markdown-mode . "melpa-stable")
         (marshal . "melpa-stable")
