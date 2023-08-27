97bdd00845e9e32119518e462feea97632d16462
purcemac_rhinoLinux <erimendz@gmail.com>
2023-08-27 17:25:58 +0300

    create get-file-name.el script

---
+++ personal/coding/get-file-name.el
@@ -0,0 +1,12 @@
+;; get-file-name.el
+;; Commentary:
+;; Code:
+;; Get user input
+(defun my/get-file-name ()
+  "Prompt user for a file name with completion and history support.
+Insert the file name at the end of buffer."
+  (interactive)
+  (insert "\n;; The file name is " (read-file-name "Enter the file name: ")))
+
+(my/get-file-name)
+;; The file name is ~/.profile
