2988991c64da22f0ca095a8efe03496458ac1f06
Bozhidar Batsov <bozhidar@toptal.com>
2026-03-14 22:24:33 +0100

    Wrap prelude-lsp-mode config in with-eval-after-load
    Defer lsp-ui setup until lsp-mode is actually loaded, avoiding
    potential issues with keymaps not being defined at load time.

--- modules/prelude-lsp-mode.el
+++ modules/prelude-lsp-mode.el
@@ -33,24 +33,25 @@
 (prelude-require-packages '(lsp-mode
                             lsp-ui))
 
-(require 'lsp-ui)
-(require 'lsp-ui-imenu)
+(with-eval-after-load 'lsp-mode
+  (require 'lsp-ui)
+  (require 'lsp-ui-imenu)
 
-(define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-(define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
-(define-key lsp-ui-mode-map (kbd "C-c C-l .") 'lsp-ui-peek-find-definitions)
-(define-key lsp-ui-mode-map (kbd "C-c C-l ?") 'lsp-ui-peek-find-references)
-(define-key lsp-ui-mode-map (kbd "C-c C-l r") 'lsp-rename)
-(define-key lsp-ui-mode-map (kbd "C-c C-l x") 'lsp-workspace-restart)
-(define-key lsp-ui-mode-map (kbd "C-c C-l w") 'lsp-ui-peek-find-workspace-symbol)
-(define-key lsp-ui-mode-map (kbd "C-c C-l i") 'lsp-ui-peek-find-implementation)
-(define-key lsp-ui-mode-map (kbd "C-c C-l d") 'lsp-describe-thing-at-point)
-(define-key lsp-ui-mode-map (kbd "C-c C-l e") 'lsp-execute-code-action)
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+  (define-key lsp-ui-mode-map (kbd "C-c C-l .") 'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map (kbd "C-c C-l ?") 'lsp-ui-peek-find-references)
+  (define-key lsp-ui-mode-map (kbd "C-c C-l r") 'lsp-rename)
+  (define-key lsp-ui-mode-map (kbd "C-c C-l x") 'lsp-workspace-restart)
+  (define-key lsp-ui-mode-map (kbd "C-c C-l w") 'lsp-ui-peek-find-workspace-symbol)
+  (define-key lsp-ui-mode-map (kbd "C-c C-l i") 'lsp-ui-peek-find-implementation)
+  (define-key lsp-ui-mode-map (kbd "C-c C-l d") 'lsp-describe-thing-at-point)
+  (define-key lsp-ui-mode-map (kbd "C-c C-l e") 'lsp-execute-code-action)
 
-(setq lsp-ui-sideline-enable t)
-(setq lsp-ui-doc-enable t)
-(setq lsp-ui-peek-enable t)
-(setq lsp-ui-peek-always-show t)
+  (setq lsp-ui-sideline-enable t)
+  (setq lsp-ui-doc-enable t)
+  (setq lsp-ui-peek-enable t)
+  (setq lsp-ui-peek-always-show t))
 
 (provide 'prelude-lsp-mode)
 ;;; prelude-lsp-mode.el ends here
