f19a455d466cdf8008542c4d3c99f8beb07a0a2b
Bozhidar Batsov <bozhidar@toptal.com>
2026-03-15 07:28:40 +0100

    Use built-in ansi-color-compilation-filter
    Replaces the custom prelude-colorize-compilation-buffer with
    the built-in filter available since Emacs 28.

--- core/prelude-editor.el
+++ core/prelude-editor.el
@@ -323,26 +323,13 @@ Does not indent if the mode is in `prelude-indent-sensitive-modes'."
       (expand-file-name "semanticdb" prelude-savefile-dir))
 
 ;; Compilation from Emacs
-(defun prelude-colorize-compilation-buffer ()
-  "Colorize a compilation mode buffer."
-  (interactive)
-  ;; we don't want to mess with child modes such as grep-mode, ack, ag, etc
-  (when (eq major-mode 'compilation-mode)
-    (let ((inhibit-read-only t))
-      (ansi-color-apply-on-region (point-min) (point-max)))))
-
 (require 'compile)
-(setq compilation-ask-about-save nil  ; Just save before compiling
-      compilation-always-kill t       ; Just kill old compile processes before
-                                        ; starting the new one
-      compilation-scroll-output 'first-error ; Automatically scroll to first
-                                        ; error
-      )
+(setq compilation-ask-about-save nil
+      compilation-always-kill t
+      compilation-scroll-output 'first-error)
 
-;; Colorize output of Compilation Mode, see
-;; http://stackoverflow.com/a/3072831/355252
-(require 'ansi-color)
-(add-hook 'compilation-filter-hook #'prelude-colorize-compilation-buffer)
+;; Colorize output of Compilation Mode
+(add-hook 'compilation-filter-hook #'ansi-color-compilation-filter)
 
 ;; enable Prelude's keybindings
 (prelude-mode t)
