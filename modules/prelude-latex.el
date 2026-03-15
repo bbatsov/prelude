6e6646ed290e756bad8322bfb71684f203540de9
Bozhidar Batsov <bozhidar@toptal.com>
2026-03-15 13:20:05 +0100

    Enable subword-mode in prelude-latex

--- modules/prelude-latex.el
+++ modules/prelude-latex.el
@@ -75,6 +75,7 @@
   "Default Prelude hook for `LaTeX-mode'."
   (turn-on-auto-fill)
   (abbrev-mode +1)
+  (subword-mode +1)
   (smartparens-mode +1)
   (cl-case prelude-latex-fast-math-entry
     (LaTeX-math-mode (LaTeX-math-mode 1))
