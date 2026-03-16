56120c684b1e035e4d3e5d66e010d1ecff57753e
Bozhidar Batsov <bozhidar@batsov.dev>
2026-03-16 23:06:53 +0200

    Guard set-fontset-font with display-graphic-p
    set-fontset-font is unavailable in terminal-only Emacs builds,
    so calling it unconditionally causes a void-function error in
    console mode.

--- core/prelude-macos.el
+++ core/prelude-macos.el
@@ -60,7 +60,8 @@ Windows external keyboard from time to time."
 (menu-bar-mode +1)
 
 ;; Enable emoji, and stop the UI from freezing when trying to display them.
-(set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend)
+(when (display-graphic-p)
+  (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend))
 
 
 (provide 'prelude-macos)
