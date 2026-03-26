522d7119904cb177999b4b881f453e1622e1c0d5
Bozhidar Batsov <bozhidar@batsov.dev>
2026-03-26 16:08:47 +0200

    Convert prelude-css to use-package

--- modules/prelude-css.el
+++ modules/prelude-css.el
@@ -30,12 +30,15 @@
 
 ;;; Code:
 
-(prelude-require-packages '(rainbow-mode))
-
 ;; Use css-ts-mode when the tree-sitter grammar is available
 (prelude-treesit-remap 'css 'css-mode 'css-ts-mode)
 
 (setq css-indent-offset 2)
+
+;; Colorize color names and hex values in CSS buffers
+(use-package rainbow-mode
+  :ensure t
+  :defer t)
 
 (defun prelude-css-mode-defaults ()
   (rainbow-mode +1)
