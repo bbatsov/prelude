b17c58dc55681b77a407036e54378f8e60ea3f03
Bozhidar Batsov <bozhidar@batsov.dev>
2026-03-26 16:17:15 +0200

    Convert prelude-web to use-package
    Use :mode for file associations, :custom for settings.
    Remove eager (require 'web-mode). Add comments explaining
    smartparens integration and auto-pairing.

--- modules/prelude-web.el
+++ modules/prelude-web.el
@@ -30,55 +30,52 @@
 
 ;;; Code:
 
-(prelude-require-packages '(web-mode))
+(defun prelude-web-mode-defaults ()
+  (subword-mode +1)
+  (run-hooks 'prelude-prog-mode-hook))
 
-(require 'web-mode)
+;; web-mode: major mode for editing web templates (HTML, PHP, ERB,
+;; Jinja, Mustache, JSP, Vue, Svelte, etc.)
+(use-package web-mode
+  :ensure t
+  :mode ("\\.phtml\\'"
+         "\\.tpl\\.php\\'"
+         "\\.tpl\\'"
+         "\\.hbs\\'"
+         "\\.blade\\.php\\'"
+         "\\.jsp\\'"
+         "\\.as[cp]x\\'"
+         "\\.erb\\'"
+         "\\.mustache\\'"
+         "\\.njk\\'"
+         "\\.jinja2?\\'"
+         "\\.svelte\\'"
+         "\\.vue\\'"
+         "\\.html?\\'"
+         "/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'")
+  :custom
+  (web-mode-markup-indent-offset 2)
+  (web-mode-css-indent-offset 2)
+  (web-mode-code-indent-offset 2)
+  ;; Let smartparens handle pairing instead of web-mode's built-in
+  (web-mode-enable-auto-pairing nil)
+  :hook (web-mode . (lambda ()
+                      (run-hooks 'prelude-web-mode-hook))))
 
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.blade\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.njk\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jinja2?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.svelte\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist
-             '("/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'" . web-mode))
-
-;; sensible indentation defaults
-(setq web-mode-markup-indent-offset 2)
-(setq web-mode-css-indent-offset 2)
-(setq web-mode-code-indent-offset 2)
-
-;; make web-mode play nice with smartparens
-(setq web-mode-enable-auto-pairing nil)
-
-(sp-with-modes '(web-mode)
-  (sp-local-pair "%" "%"
-                 :unless '(sp-in-string-p)
-                 :post-handlers '(((lambda (&rest _ignored)
-                                     (just-one-space)
-                                     (save-excursion (insert " ")))
-                                   "SPC" "=" "#")))
-  (sp-local-tag "%" "<% "  " %>")
-  (sp-local-tag "=" "<%= " " %>")
-  (sp-local-tag "#" "<%# " " %>"))
-
+;; smartparens integration for ERB/EJS-style template tags
 (with-eval-after-load 'web-mode
-  (defun prelude-web-mode-defaults ()
-    (subword-mode +1)
-    (run-hooks 'prelude-prog-mode-hook))
+  (sp-with-modes '(web-mode)
+    (sp-local-pair "%" "%"
+                   :unless '(sp-in-string-p)
+                   :post-handlers '(((lambda (&rest _ignored)
+                                       (just-one-space)
+                                       (save-excursion (insert " ")))
+                                     "SPC" "=" "#")))
+    (sp-local-tag "%" "<% "  " %>")
+    (sp-local-tag "=" "<%= " " %>")
+    (sp-local-tag "#" "<%# " " %>")))
 
-  (setq prelude-web-mode-hook 'prelude-web-mode-defaults)
-
-  (add-hook 'web-mode-hook (lambda ()
-                             (run-hooks 'prelude-web-mode-hook))))
+(setq prelude-web-mode-hook 'prelude-web-mode-defaults)
 
 (provide 'prelude-web)
 ;;; prelude-web.el ends here
