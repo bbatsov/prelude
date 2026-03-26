a3f86c49c80b6599ea2df81fbecc4fb11f78a185
Bozhidar Batsov <bozhidar@batsov.dev>
2026-03-26 16:14:59 +0200

    Modernize prelude-latex
    - Convert to use-package
    - Load company-auctex only when company is present (:after company)
    - Replace cl-case with pcase (drop cl-lib require)
    - Remove redundant TeX-PDF-mode setting (default since AUCTeX 11.88)
    - Add comments explaining non-obvious settings

--- modules/prelude-latex.el
+++ modules/prelude-latex.el
@@ -30,14 +30,24 @@
 
 ;;; Code:
 
-(prelude-require-packages '(auctex cdlatex))
 (require 'smartparens-latex)
-;; for case
-(require 'cl-lib)
 
-(with-eval-after-load "company"
-  (prelude-require-packages '(company-auctex))
-  (company-auctex-init))
+;; AUCTeX: the de facto standard LaTeX editing environment for Emacs
+(use-package auctex
+  :ensure t
+  :defer t)
+
+;; Fast math symbol entry via backtick shortcuts (e.g., ` a → \alpha)
+(use-package cdlatex
+  :ensure t
+  :defer t)
+
+;; Company completion for AUCTeX (labels, citations, macros, etc.)
+(use-package company-auctex
+  :ensure t
+  :defer t
+  :after company
+  :config (company-auctex-init))
 
 (defcustom prelude-latex-fast-math-entry 'LaTeX-math-mode
   "Method used for fast math symbol entry in LaTeX."
@@ -51,13 +61,14 @@
 ;; AUCTeX configuration
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
+
+;; Disable smart quote insertion -- many users prefer to manage
+;; quotes manually or via a snippet system
 (setq TeX-close-quote "")
 (setq TeX-open-quote "")
 
+;; Ask for the master file when editing multi-file documents
 (setq-default TeX-master nil)
-
-;; use pdflatex
-(setq TeX-PDF-mode t)
 
 ;; sensible defaults for macOS, other OSes should be covered out-of-the-box
 (when (eq system-type 'darwin)
@@ -77,9 +88,9 @@
   (abbrev-mode +1)
   (subword-mode +1)
   (smartparens-mode +1)
-  (cl-case prelude-latex-fast-math-entry
-    (LaTeX-math-mode (LaTeX-math-mode 1))
-    (cdlatex (turn-on-cdlatex))))
+  (pcase prelude-latex-fast-math-entry
+    ('LaTeX-math-mode (LaTeX-math-mode 1))
+    ('cdlatex (turn-on-cdlatex))))
 
 (setq prelude-latex-mode-hook 'prelude-latex-mode-defaults)
 
